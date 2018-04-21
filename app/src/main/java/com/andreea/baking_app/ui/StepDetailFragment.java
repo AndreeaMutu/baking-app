76fe266251c359f12d05a3f92e2d0445117fbfb9
Andreea <andreea.mutu3@gmail.com>
2018-04-21 15:27:15 +0300

    scroll view on phone layout for details

--- app/src/main/java/com/andreea/baking_app/ui/StepDetailFragment.java
+++ app/src/main/java/com/andreea/baking_app/ui/StepDetailFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,7 +54,6 @@ public class StepDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("RecipeFrag", "onCreateView: " + (savedInstanceState != null));
         View rootView = inflater.inflate(R.layout.step_detail, container, false);
         ButterKnife.bind(this, rootView);
 
